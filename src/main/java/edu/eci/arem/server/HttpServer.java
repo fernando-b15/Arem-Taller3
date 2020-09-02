3e24ea9de76b66b33484b0a6648d252d535b6393
fernando <f9830@hotmail.com>
2020-09-02 06:16:18 -0500

    arreglando heroku 2 :wolf:

--- src/main/java/edu/eci/arem/server/HttpServer.java
+++ src/main/java/edu/eci/arem/server/HttpServer.java
@@ -148,7 +148,7 @@ public class HttpServer {
 						+"            <div class='col-md-12 text-center'>"
 						+"               <span class='display-1 d-block'>404</span>"
 						+"                <div class='mb-4 lead'>The page you are looking for was not found.</div>"
-						+"                <a href='http://localhost:36000' class='btn btn-link'>Back to Home</a>"
+						+"                <a href='https://arem-taller3.herokuapp.com/' class='btn btn-link'>Back to Home</a>"
 						+"           </div>"
 						+"       </div>"
 						+"   </div>"
